494453218339488987301a9a4a75321fa1d95956
ppaulo22 <pedro2206@live.com>
2022-01-30 20:24:17 -0300

    Página de detalhes do animal.

--- src/app/animais/animais-routing.module.ts
+++ src/app/animais/animais-routing.module.ts
@@ -1,11 +1,17 @@
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: ':animalId',
     component: ListaAnimaisComponent,
+  },
+
+  {
+    path: '',
+    component: DetalheAnimalComponent,
   }
 ];
 
