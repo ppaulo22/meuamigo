46d5959ecf5b0afba46a2098a260f496c0ed1b48
ppaulo22 <pedro2206@live.com>
2022-02-13 23:26:03 -0300

    Adicionando Resolver (para carregamento mais rápido)

---
+++ src/app/animais/detalhe-animal/comentarios/comentarios.service.ts
@@ -0,0 +1,25 @@
+import { Observable } from 'rxjs';
+import { Comentarios, Comentario } from './comentarios';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../../../environments/environment';
+import { Injectable } from '@angular/core';
+
+const API = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComentariosService {
+
+  constructor(private http: HttpClient) { }
+
+  buscaComentario(id: number): Observable<Comentarios> {
+    return this.http.get<Comentarios>(`${API}/photos/${id}/comments`);
+  }
+
+  incluiComentario(id: number, commentText: string): Observable<Comentario> {
+    return this.http.post<Comentario>(`${API}/photos/${id}/comments` , {
+      commentText,
+    });
+  }
+}
